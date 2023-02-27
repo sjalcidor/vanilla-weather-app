29eb8811599d256c78275910e9ca966d53287092
sjalcidor <120965448+sjalcidor@users.noreply.github.com>
2023-02-26 20:05:00 -0500

    add temp conversion

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//Calculate Date and Time
 let now = new Date();
 let h3 = document.querySelector("h3");
 let days = [
@@ -37,6 +38,7 @@ function formatDate(timestamp) {
 
 h3.innerHTML = `${day}, ${month} ${date} ${hour}:${minutes}`;
 
+//Displaying city name when searched
 function search(event) {
   event.preventDefault();
   let cities = document.querySelector("#city");
@@ -46,16 +48,23 @@ function search(event) {
 let searchBar = document.querySelector("#search-form");
 searchBar.addEventListener("submit", search);
 
+//Temperature Converison
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = 66;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
 function convertToCelsius(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = 19;
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit");
@@ -64,6 +73,7 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", convertToCelsius);
 
+//Displaying Temperature, humidity, wind & description
 function showTemp(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
@@ -90,8 +100,9 @@ function citySearcher(city) {
   axios.get(apiUrl).then(showTemp);
 }
 
+//Searching for exact geolocation
 function searchLocation(position) {
-  let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+  let apiKey = "20811777afcctfb386aebf0oe8a480fb";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(searchLocation);
@@ -106,4 +117,5 @@ currentLocation.addEventListener("click", getCurrentLocation);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submit);
 
+//Default City
 citySearcher("New York City");
