8ed9db92352ef3610753fcf05546a6432792c606
sjalcidor <120965448+sjalcidor@users.noreply.github.com>
2023-02-27 20:42:25 -0500

    update index

--- src/app.js
+++ src/app.js
@@ -102,7 +102,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index <6){
+    if (index < 6){
     forecastHTML =
       forecastHTML +
       `
