6122cf37698f8a033b33b1870a27dffb09f8e283
sjalcidor <120965448+sjalcidor@users.noreply.github.com>
2023-02-27 19:58:57 -0500

    display forecast

--- src/app.js
+++ src/app.js
@@ -101,7 +101,6 @@ function showTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  getForecast(response.data.coord);
 }
 
 function submit(event) {
@@ -136,42 +135,37 @@ searchForm.addEventListener("submit", submit);
 
 //Display Forecast
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
-  return days[day];
-}
-function displayForecast(response) {
-  let forecast = response.data.daily;
+function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        ` 
-              <div class="col-2">
-               <div class="forecast-date">${formatDay(forecastDay.dt)}</div> 
-                <img src="http://openweathermap.org/img/wn/${
-                  forecastDay.weather[0].icon
-                }@2x.png" alt=""/>
-                <div class="weather-forecast-temp">
-                  <span class="weather-temp-max">${Math.round(
-                    forecastDay.temp.max
-                  )}° </span>
-                  <span class="weather-temp-min">${Math.round(
-                    forecastDay.temp.min
-                  )}°</span>
-                </div>
-              </div>
-            `;
-    }
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 //Default City
 citySearcher("New York City");
+displayForecast();
